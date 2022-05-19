911855fea871d0b23384d96644b928757fa35a65
Emily Reed <emilyreed89@gmail.com>
2022-05-19 12:54:39 -0400

    my config

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    MY_API_TOKEN : '12345', //435a0150-7a8a-4f06-a720-0ca6979e4d06
+    SECRET_API_KEY : '435a0150-7a8a-4f06-a720-0ca6979e4d06'
+  }
+
+let config2 = {
+    MY_API_KEY2: 'vH37jzPPJ0lo0jKajzO5lUznozYEhqoJycbPWJ4Y',
+    SECRET_API2 : 'Ore2no3bqTec2s8k4M3Nzh3zuoK0t2N0pllkl5lp2evOOukxUL'
+}
